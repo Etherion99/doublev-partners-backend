b974bac50a2daaf95a8d0bd15696cedd55d76639
devops <devops@smartsoftint.com>
2023-06-15 23:47:58 -0500

    create cruds routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TicketController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// users CRUD
+Route::get('/users', [UserController::class, 'all']); // Get all users
+Route::post('/users', [UserController::class, 'create']); // Create a new user
+Route::get('/users/{id}', [UserController::class, 'get']); // Get a specific user
+Route::put('/users/{id}', [UserController::class, 'update']); // Update a user
+Route::delete('/users/{id}', [UserController::class, 'remove']); // Delete a user
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Tickets CRUD
+Route::get('/tickets', [TicketController::class, 'all']); // Get all tickets
+Route::post('/tickets', [TicketController::class, 'create']); // Create a new ticket
+Route::get('/tickets/{id}', [TicketController::class, 'get']); // Get a specific ticket
+Route::put('/tickets/{id}', [TicketController::class, 'update']); // Update a ticket
+Route::delete('/tickets/{id}', [TicketController::class, 'remove']); // Delete a ticket
